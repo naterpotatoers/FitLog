6f05e5a5d58b413527ea558c51419abe911fb9d5
naterpotatoer <nate.garza@hotmail.com>
2023-05-26 12:07:55 -0700

    overhauls css, adds AddWorkout component w/ styling

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import AddWorkout from './AddWorkout'
 import WorkoutHistory from './WorkoutHistory'
 
 export default function Dashboard() {
     return (
         <div>
+            <AddWorkout />
             <WorkoutHistory />
         </div>
     )
