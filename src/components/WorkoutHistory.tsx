70ff415f587cc2c62af773e7b16317ac0a16508c
naterpotatoer <nate.garza@hotmail.com>
2023-05-26 23:55:57 -0700

    adds firebase logic, weird query bug

--- src/components/WorkoutHistory.tsx
+++ src/components/WorkoutHistory.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { formatCreatedDate } from '../common/formatCreatedDate'
 import { WorkoutsDTO, MOCK_WORKOUTS } from '../dto/workouts.dto'
+import { getDocs, collection } from "firebase/firestore";
+import { db } from '../config/firebase';
 
 export default function WorkoutHistory() {
     // eslint-disable-next-line
     const [workouts, setWorkouts] = React.useState<WorkoutsDTO[]>(MOCK_WORKOUTS)
+    // const [journals, setJournals] = React.useState<any[]>([])
+
+    const collectionRef = collection(db, "journals");
+
+    useEffect(() => {
+        const getJournals = async () => {
+            try {
+                const result = await getDocs(collectionRef);
+                console.log(result)
+            }
+            catch (e) {
+                console.log(e)
+            }
+        }
+        getJournals()
+    }, [collectionRef])
 
     const groupedWorkouts = workouts.reduce((acc, workout) => {
         let date = formatCreatedDate(workout.created_at)
