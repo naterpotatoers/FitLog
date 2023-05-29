ac972c6bff9c454c4fe1a92989f7b0e4279766e0
naterpotatoer <nate.garza@hotmail.com>
2023-05-28 20:41:37 -0700

    moves firestore files to new dir, adds update form w/ no logic

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import AddStrengthSet from '../components/Forms/AddStrengthSet'
+import UpdateStrengthSet from '../components/Forms/UpdateStrengthSet'
 import WorkoutHistory from '../components/WorkoutHistory'
 
 export default function Home() {
   return (
     <div className='grid'>
       <AddStrengthSet />
+      <UpdateStrengthSet />
       <WorkoutHistory />
     </div>
   )
