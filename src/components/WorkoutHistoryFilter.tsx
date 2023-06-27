06423d5ef83936024bc26953a19be8e7c4f8ec6d
naterpotatoer <nate.garza@hotmail.com>
2023-06-27 15:35:50 -0700

    fixes filter styling

--- src/components/WorkoutHistoryFilter.tsx
+++ src/components/WorkoutHistoryFilter.tsx
@@ -2,14 +2,18 @@ import { ExerciseDTO, MOCK_EXERCISES } from '../dto/Exercise.dto';
 
 export default function WorkoutHistoryFilter({ handleJournalFilter }) {
     return (
-        <div className='card'>
-            <label htmlFor='filter'>Filter by exercise:</label>
-            <select name='filter' id='filter' onChange={handleJournalFilter} className='form-input'>
-                {MOCK_EXERCISES.map((exercise: ExerciseDTO) => {
-                    return <option key={exercise.id} value={exercise.id}>{exercise.id}</option>
-                }
-                )}
-            </select>
+        <div className='grid'>
+            <div className='card'>
+                <div className='form-item'>
+                    <label htmlFor='filter'>Filter by exercise:</label>
+                    <select name='filter' id='filter' onChange={handleJournalFilter} className='form-input'>
+                        {MOCK_EXERCISES.map((exercise: ExerciseDTO) => {
+                            return <option key={exercise.id} value={exercise.id}>{exercise.id}</option>
+                        }
+                        )}
+                    </select>
+                </div>
+            </div>
         </div>
     )
 }
